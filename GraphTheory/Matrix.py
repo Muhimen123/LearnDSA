2eac83f8d5603f40297cc7248ad0ebd1210064f7
Al Muhimen <almuhimen4@gmail.com>
2020-05-29 20:46:35 +0600

    Added GraphTheory

---
+++ GraphTheory/Matrix.py
@@ -0,0 +1,46 @@
+class Graph:
+    def __init__(self, size):
+        self.Matrix = []
+        for _ in range(size):
+            self.tmp = [0] * size
+            self.Matrix.append(self.tmp)
+
+    def add_edge(self, v1, v2):
+        if v1 == v2:
+            print("Both of them are the same")
+        else:
+            v1 -= 1
+            v2 -= 1
+            self.Matrix[v2][v1] = 1
+            self.Matrix[v1][v2] = 1
+
+    def delete_edge(self, v1, v2):
+        if self.Matrix[v2-1][v1-1] == 0:
+            print("The is no vertex to delete")
+        else:
+            v1 -= 1
+            v2 -= 1
+            self.Matrix[v2][v1] = 0
+            self.Matrix[v1][v2] = 0
+
+    def show_graph(self):
+        for self.row in self.Matrix:
+            print(self.row)
+
+
+my_graph = Graph(10)
+my_graph.add_edge(5, 6)
+my_graph.add_edge(1, 8)
+my_graph.add_edge(1, 9)
+my_graph.add_edge(1, 7)
+my_graph.add_edge(9, 8)
+
+print("Before deleting something\n")
+my_graph.show_graph()
+
+my_graph.delete_edge(2, 8)
+my_graph.delete_edge(5, 6)
+my_graph.delete_edge(1, 7)
+
+print("After deleting something\n")
+my_graph.show_graph()
